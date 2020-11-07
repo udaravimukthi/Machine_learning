8e5af4c829f27af10986413ba28cc11f451095b2
udara <uvlakshan@gmail.com>
2020-11-07 21:53:20 +0530

    connect to data set

---
+++ r_data_preprocessing.R
@@ -0,0 +1,9 @@
+mydataset = read.csv('mydata.csv')
+
+mydataset$Age = ifelse(is.na(mydataset$Age),ave(mydataset$Age, FUN = function(x)
+  mean(x,na.rm =TRUE)),
+  mydataset$Age)
+
+mydataset$Income = ifelse(is.na(mydataset$Income),ave(mydataset$Income, FUN = function(x)
+  mean(x,na.rm =TRUE)),
+  mydataset$Income)
