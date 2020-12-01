0e1cf2fff467e15a613a80c366a92bd194f24526
udara <uvlakshan@gmail.com>
2020-12-01 23:33:15 +0530

    chsnged line

--- r_data_preprocessing.R
+++ r_data_preprocessing.R
@@ -7,3 +7,5 @@ mydataset$Age = ifelse(is.na(mydataset$Age),ave(mydataset$Age, FUN = function(x)
 mydataset$Income = ifelse(is.na(mydataset$Income),ave(mydataset$Income, FUN = function(x)
   mean(x,na.rm =TRUE)),
   mydataset$Income)
+
+  
